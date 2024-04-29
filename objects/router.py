a9fb069cbb52e95e0f966dc3c23d7975b6306793
PePacheco <pedro_pacheco98@outlook.com>
2024-04-29 18:21:21 -0300

    Started mping

--- objects/router.py
+++ objects/router.py
@@ -15,5 +15,5 @@ class Router:
   def receive_from_subnet(self, subnet_id, mgroupid, msg):
       print(f"{subnet_id} =>> {self.rid} : mping {mgroupid} {msg}")
       for sid, subnet in self.subnets.items():
-          if sid != subnet_id: # Nao envia para a subnet que recebeu
+          if sid != subnet_id and subnet.isOnGroup(mgroupid): # Nao envia para a subnet que recebeu
             subnet.receive_from_router(self.rid, mgroupid, msg)
