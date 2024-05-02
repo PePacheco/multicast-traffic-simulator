77d25f1bf80be615a6e279f2463bc5bd7af3734c
Heitor Kunrath <60748906+HeitorKun@users.noreply.github.com>
2024-05-01 22:12:33 -0300

    adding all routers to Router Center

--- objects/RouterCenter.py
+++ objects/RouterCenter.py
@@ -10,12 +10,12 @@ class RouterCenter:
     def __init__(self):
         if RouterCenter.__instance is not None:
             raise Exception("RouterCenter is a singleton class!")
-        else:
-            RouterCenter.__instance = self
+        RouterCenter.__instance = self
         self.routers = {}
 
     def add_router(self, router):
-        self.routers[router.ip] = router
+        for ip in router.ips:
+            self.routers[ip] = router
 
     def get_routers(self):
         return self.routers
