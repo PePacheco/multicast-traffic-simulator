0f38ed448d6cbfa2a5b7eb9b4458140e87c82eff
Heitor Kunrath <60748906+HeitorKun@users.noreply.github.com>
2024-05-14 15:39:50 -0300

    added last_adress to baseMessage

--- message_types/base_message.py
+++ message_types/base_message.py
@@ -1,10 +1,14 @@
 class BaseMessage:
-    def __init__(self, origin_adress, destination_adress):
+    def __init__(self, origin_adress, destination_adress, last_adress):
         self.origin_adress = origin_adress
         self.destination_adress = destination_adress
+        self.last_adress = last_adress
 
     def get_origin_adress(self):
         return self.origin_adress
     
     def get_destination_adress(self):
         return self.destination_adress
+    
+    def get_last_adress(self):
+        return self.last_adress
